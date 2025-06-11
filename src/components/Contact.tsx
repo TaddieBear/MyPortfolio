819034ba3a09d96041ce9068cfde35b6d15fac0c
TaddieBear <tradhcordez@gmail.com>
2025-06-12 02:13:14 +0800

    changed phone number format in Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -67,8 +67,8 @@ const contactInfo = [
   {
     icon: Phone,
     label: "Phone",
-    value: "+1 (555) 123-4567",
-    href: "tel:+15551234567",
+    value: "+63 905 418 2060",
+    href: "cel no.:+63 905 418 2060",
   },
   {
     icon: MapPin,
